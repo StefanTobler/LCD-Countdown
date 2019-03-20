cdbf6607cabf6db78755812501e4685b1035296e
Stefan Tobler <toblerlstefan@gmail.com>
2019-03-20 23:15:41 +0000

    Fixed formatting

--- countdown.py
+++ countdown.py
@@ -40,11 +40,7 @@ try:
         minutes = int(math.floor(temp_time / 60))
         seconds = int(temp_time - minutes * 60)
         
-        minutes_string = "0{}".format(minutes) if minutes < 10 else "{}".format(minutes)
-        days_string = "0{}".format(days) if days < 10 else "{}".format(days)
-        hours_string = "0{}".format(hours) if hours < 10 else "{}".format(hours)
-        seconds_string = "0{}".format(seconds) if seconds < 10 else "{}".format(seconds)
-        lcd.lcd_string("  {}:{}:{}:{}".format(days_string,hours_string,minutes_string,seconds_string), LINE_2)
+        lcd.lcd_string("  %02d:%02d:%02d:%02d" % (days,hours,minutes,seconds), LINE_2)
 
-except KeyBoardInterrupt:
+except KeyboardInterrupt:
     GPIO.cleanup() # cleaning up GPIO pins
